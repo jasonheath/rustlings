fe7ffef69c77115917baeb42385f3113e9d0caec
Jason Heath <jh@jasonheath.com>
2022-08-21 11:08:11 -0400

    completed the options exercises

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -1,8 +1,6 @@
 // options1.rs
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 // you can modify anything EXCEPT for this function's signature
 fn print_number(maybe_number: Option<u16>) {
     println!("printing: {}", maybe_number.unwrap());
@@ -11,11 +9,14 @@ fn print_number(maybe_number: Option<u16>) {
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
 // all, so there'll be no more left :(
-// TODO: Return an Option!
-fn maybe_icecream(time_of_day: u16) -> Option<u16> {
+fn maybe_ice_cream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22
     // The Option output should gracefully handle cases where time_of_day > 24.
-    ???
+    match time_of_day {
+        0..=21 => Some(5),
+        22..=24 => Some(0),
+        _ => None,
+    }
 }
 
 #[cfg(test)]
@@ -23,17 +24,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check_icecream() {
-        assert_eq!(maybe_icecream(10), Some(5));
-        assert_eq!(maybe_icecream(23), Some(0));
-        assert_eq!(maybe_icecream(22), Some(0));
-        assert_eq!(maybe_icecream(25), None);
+    fn check_ice_cream() {
+        assert_eq!(maybe_ice_cream(10), Some(5));
+        assert_eq!(maybe_ice_cream(23), Some(0));
+        assert_eq!(maybe_ice_cream(22), Some(0));
+        assert_eq!(maybe_ice_cream(25), None);
     }
 
     #[test]
     fn raw_value() {
-        // TODO: Fix this test. How do you get at the value contained in the Option?
-        let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        let ice_creams = maybe_ice_cream(12).unwrap_or(255);
+        assert_eq!(ice_creams, 5);
     }
 }
